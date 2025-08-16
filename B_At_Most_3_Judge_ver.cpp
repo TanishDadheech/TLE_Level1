eeb534ccbc8bf2984e13c9211115e5bea26f1e3d
TanishDadheech <tdadheech07@gmail.com>
2025-08-16 13:57:13 +0530

    Day2 Q1.

--- B_At_Most_3_Judge_ver.cpp
+++ B_At_Most_3_Judge_ver.cpp
@@ -5,28 +5,42 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-    cin >> N;
-    string T;
-    cin >> T;
+    int N, W;
+    cin >> N >> W;
 
-    long long x = 0, y = 0;
-    char dir = 'E'; // E, S, W, N
+    int A[305];               // since N ≤ 300
+    bool good[1000005] = {0}; // since W ≤ 10^6
 
-    for (char c : T) {
-        if (c == 'S') {
-            if (dir == 'E') x++;
-            else if (dir == 'S') y--;
-            else if (dir == 'W') x--;
-            else if (dir == 'N') y++;
-        } else if (c == 'R') {
-            if (dir == 'E') dir = 'S';
-            else if (dir == 'S') dir = 'W';
-            else if (dir == 'W') dir = 'N';
-            else if (dir == 'N') dir = 'E';
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    // 1 weight
+    for (int i = 0; i < N; i++) {
+        if (A[i] <= W) good[A[i]] = true;
+    }
+
+    // 2 weights
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            long long sum = 1LL * A[i] + A[j];
+            if (sum <= W) good[sum] = true;
         }
     }
 
-    cout << x << " " << y << "\n";
+    // 3 weights
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            for (int k = j + 1; k < N; k++) {
+                long long sum = 1LL * A[i] + A[j] + A[k];
+                if (sum <= W) good[sum] = true;
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int n = 1; n <= W; n++) {
+        if (good[n]) ans++;
+    }
+
+    cout << ans << "\n";
     return 0;
 }
